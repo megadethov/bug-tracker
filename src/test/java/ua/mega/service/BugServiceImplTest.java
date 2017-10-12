7feb5f5e0bdbca86b1f6228ad2a998dbf6829dda
Yuriy Yefremov <megadethov@gmail.com>
2017-10-12 10:18:45 +0300

    add JUnit and Spring Test

---
+++ src/test/java/ua/mega/service/BugServiceImplTest.java
@@ -0,0 +1,43 @@
+package ua.mega.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BugServiceImplTest {
+    @Test
+    public void createNewBug() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void updateBug() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void deleteBug() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void getAllBugs() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void getAllBugsByAssignee() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void getAllBugsByReporter() throws Exception {
+        fail();
+    }
+
+}
---
+++ src/test/java/ua/mega/service/PersonServiceImplTest.java
@@ -0,0 +1,28 @@
+package ua.mega.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration("classpath:spring/spring-app.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonServiceImplTest {
+    @Test
+    public void createNewPerson() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void updatePerson() throws Exception {
+        fail();
+    }
+
+    @Test
+    public void getAllPersons() throws Exception {
+        fail();
+    }
+
+}
